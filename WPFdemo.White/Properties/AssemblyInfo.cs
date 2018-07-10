ae5f24593edd30ffcd2b8fda254d8f0f5368d5a2
farhan shaikh <shaikh.farhan2009@gmail.com>
2018-07-10 13:47:57 +0100

    Add project files.

---
+++ WPFdemo.White/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WPFdemo.White")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Amazon.com")]
+[assembly: AssemblyProduct("WPFdemo.White")]
+[assembly: AssemblyCopyright("Copyright © Amazon.com 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa35f78f-92d7-4f36-8608-d19d9cb99067")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WPFdemo.White/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+using System.Diagnostics;
+
+namespace WPFdemo.White
+{
+    [TestClass]
+
+    public class UnitTest1
+    {
+        
+        public Application app;
+        [TestMethod]
+        public void InvokeApplication()
+        {
+            //Launching the application
+            string apppath = @"C:\Windows\system32\Calc.exe";
+            var prc = new ProcessStartInfo(apppath);
+            app = Application.Launch(prc);
+
+            //Getting the window object 
+            Window WndCalc = (Window)app.GetWindow("Calculator");
+
+            //Getting the controls
+            //By Automation id
+            Button btnFour = WndCalc.Get<Button>(SearchCriteria.ByAutomationId("134"));
+            //By Text
+            Button btnFive = WndCalc.Get<Button>(SearchCriteria.ByText("5"));
+            Button btnMultiply = WndCalc.Get<Button>(SearchCriteria.ByAutomationId("92"));
+            //By automation id and index
+            Button btnEquals = WndCalc.Get<Button>(SearchCriteria.ByAutomationId("121").AndIndex(0));
+
+            //Performing calculation
+            btnFour.Click();
+            btnMultiply.Click();
+            btnFive.Click();
+            btnEquals.Click();
+            Label txtResult = WndCalc.Get<Label>(SearchCriteria.ByAutomationId("158"));
+            //Assert will check if the expected equals Actual
+            Assert.AreEqual("20", txtResult.Name);
+
+
+           
+        }
+    }
+}
+
